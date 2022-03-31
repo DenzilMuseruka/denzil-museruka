b2811b7db5f949a181e1b1a39e98f39e6123207e
Dirk Dhure <ext.dirk.dhure@veneka.com>
2022-03-31 16:31:34 +0200

    404 page

---
+++ src/app/mock-api/common/user/data.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export const user = {
+    id    : 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
+    name  : 'Denzil Museruka',
+    email : 'denzilmuseruka@outlook.com',
+    avatar: 'https://pbs.twimg.com/profile_images/1465092826857889804/mddtdXPf_400x400.jpg',
+    status: 'online'
+};
